e9f447196a541c2f7371e35888e88d43c27aa676
David Nebinger <david.nebinger@liferay.com>
2024-08-02 09:10:24 -0400

    Creation of the sample portlet

---
+++ modules/langchain4j-web/src/main/java/com/liferay/community/langchain4j/sample/constants/Langchain4jWebPortletKeys.java
@@ -0,0 +1,11 @@
+package com.liferay.community.langchain4j.sample.constants;
+
+/**
+ * @author dnebinger
+ */
+public class Langchain4jWebPortletKeys {
+
+	public static final String LANGCHAIN4JWEB =
+		"com_liferay_community_langchain4j_sample_Langchain4jWebPortlet";
+
+}
---
+++ modules/langchain4j-web/src/main/java/com/liferay/community/langchain4j/sample/portlet/Langchain4jWebPortlet.java
@@ -0,0 +1,29 @@
+package com.liferay.community.langchain4j.sample.portlet;
+
+import com.liferay.community.langchain4j.sample.constants.Langchain4jWebPortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author dnebinger
+ */
+@Component(
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=Langchain4jWeb",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + Langchain4jWebPortletKeys.LANGCHAIN4JWEB,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class Langchain4jWebPortlet extends MVCPortlet {
+}
